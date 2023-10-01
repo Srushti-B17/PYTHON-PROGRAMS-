2320f0e01fb3aec856c058036ac56973337278c2
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-10-01 20:13:10 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,21 @@
+#variables and identifiers
+#implicit type conversion
+a="hello"
+b=12
+c=2+3j
+print(type(a))
+print(type(b))
+print(type(c))
+
+#explicit type conversion
+
+x=4
+n=float(x)
+print(type(n))
+
+y=int(2.4)
+print(type(y))
+
+z=complex(7.8)
+print(z)
+print(type(z))
